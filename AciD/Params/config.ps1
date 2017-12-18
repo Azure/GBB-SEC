ff185bd5494e4b4008667004d158f30051c11844
Scott Moore <scott.moore@microsoft.com>
2017-12-18 09:33:51 -0600

    AD Searcher
    Run through AD, pingcheck, report back.

---
+++ AciD/Params/config.ps1
@@ -0,0 +1,4 @@
+﻿$SearchRoot = [ADSI]"LDAP://CN=Computers,DC=Contoso,DC=Corp"
+$Filter = "(objectcategory=computer)"
+$Properties = "name"  
+$ICMPCheck = ""
---
+++ AciD/run.ps1
@@ -0,0 +1,13 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+Set-Location C:\[$ClientFolder]
+. .\Params\config.ps1
+import-module "C:\Program Files (x86)\AWS Tools\PowerShell\AWSPowerShell\AWSPowerShell.psd1"
+
+$Searcher = New-Object DirectoryServices.DirectorySearcher($SearchRoot, $Filter)  
+$Searcher.PageSize = 1000  
+$Searcher.PropertiesToLoad.AddRange($Properties)  
+$Searcher.FindAll() | Select-Object `
+@{n='Name';e={ $_.Properties["name"][0] }} ,
+@{n='UP?';e={ if(test-connection $_.Properties["name"][0] -q -count 2) {$True} Else {$False} }}  | ConvertTo-Csv -NoTypeInformation | % { $_ -replace '"', ""}  | out-file $ICMPCheck -fo -en ascii
+$filename= $ICMPCheck
+$outputfile="$filename" + ".log"
