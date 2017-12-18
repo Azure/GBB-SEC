560f97d5741e769d99cc6a977f6a1c17fb8568d3
Scott Moore <scott.moore@microsoft.com>
2017-12-18 12:11:37 -0600

    Consolidating

--- AciD/Params/az-psm-install.ps1
+++
@@ -1,4 +0,0 @@
-﻿Get-Module PowerShellGet -list | Select-Object Name,Version,Path #Check to see if Azure Powershell module is installed/version rev
-#Uncomment below if you need to install Azure Powershell module
-#Install-Module AzureRM -AllowClobber 
-Import-Module AzureRM #Run PS w/Azure Module
